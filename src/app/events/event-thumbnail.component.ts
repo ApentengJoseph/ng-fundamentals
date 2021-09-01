568bfda2aee2e69380ac1aaf4c43f701e6298f57
Apenteng Joseph <apentengjoseph59@gmail.com>
2021-09-01 13:29:19 +0100

    add new vendor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -20,7 +20,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 
 export class EventThumbnailComponent {
-    @Input() event:any
+    @Input()  event:any
     @Output() eventClick = new EventEmitter()
 
 }
